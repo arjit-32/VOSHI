e65d3eaa95fbedc923f455ac0d408e6bf29c3212
arjit321 <arjit_sharma@canalys.com>
2025-04-02 21:57:19 +0530

    banner ohmyvoshi added

--- src/main.c
+++ src/main.c
@@ -14,7 +14,6 @@
 #define YELLOW "\033[1;33m"
 #define MAGENTA "\033[1;35m"
 #define RED "\033[1;31m"
-#define WHITE "\033[1;37m"
 #define RESET "\033[0m"
 #define BOLD "\033[1m"
 
@@ -87,6 +86,9 @@ void show_loading(const char *message, int duration) {
 
 void show_dashboard() {
     system("clear");
+     // Huge ASCII Art
+    system("figlet -f big 'Oh My Voshi!'");
+
     printf(GREEN BOLD "\U0001F4CA  voshi Dashboard\n" RESET);
     printf("============================================\n");
 
