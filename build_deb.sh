c5a1cd20e28dffcecd72985573bc10fdf864b8fb
arjit321 <arjit_sharma@canalys.com>
2025-04-15 11:25:55 +0530

    changes

--- build_deb.sh
+++ build_deb.sh
@@ -2,12 +2,19 @@
 
 set -e
 
+echo "[*] Cleaning..."
+make clean
+
 echo "[*] Compiling voshi..."
 make
 
-echo "[*] Copying binary to package structure..."
+echo "[*] Preparing package structure..."
 mkdir -p package/usr/local/bin
 cp voshi package/usr/local/bin/voshi
+
+echo "[*] Setting permissions..."
+chmod 755 package/usr/local/bin/voshi
+chmod 755 package/DEBIAN
 
 echo "[*] Building .deb package..."
 dpkg-deb --build package voshi_1.0.0_amd64.deb
